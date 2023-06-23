79f72a8f6bd40bf022c3008680bb051e35b8ece7
OB97 <ajobrien@upei.ca>
2023-06-22 22:24:56 -0300

    Updated 'COLOR_ALIVE_NEXT'

--- sand.py
+++ sand.py
@@ -9,7 +9,7 @@ import numpy as np
 COLOR_BG = (10, 10, 10)
 COLOR_GRID = (40, 40, 40)
 COLOR_DIE_NEXT = (255, 255, 0)
-COLOR_ALIVE_NEXT = (102, 102, 0)
+COLOR_ALIVE_NEXT = (255, 255, 0)
 
 SCREEN = pygame.display.set_mode((800, 600))
 EMPTY_GRID = np.zeros((60, 80))
