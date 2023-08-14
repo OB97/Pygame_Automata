770e92c30277ab9f53d0053d633a25cb9a5c2257
OB97 <ajobrien@upei.ca>
2023-08-14 16:42:35 -0300

    Making progress, edited GoL, Automaton, and GameBoard

--- Objects/Automaton.py
+++ Objects/Automaton.py
@@ -3,37 +3,21 @@ import numpy as np
 
 class Automaton(object):
 
-    def __init__(self, size, location):
-        self.size = size
-        self.location = location
+    def __init__(self):
         self.status = 0
-        self.colour = None
-        self.COLOUR_DIE = (170, 170, 170)
-        self.COLOUR_ALIVE = (255, 255, 255)
 
-    def getSize(self):
-        return self.size
-
-    def getLocation(self):
-        return self.location
-
-    def nextMove(self):
-        return self
-
-    def allMoves(self):
-        return self
+    def __repr__(self):
+        return "Automaton Object"
 
     def getStatus(self):
         return self.status
 
-    def setStatus(self, inp):
-        self.status = inp
-
-    def getColour(self):
+    def switchStatus(self):
         if self.status == 0:
-            return self.COLOUR_DIE
+            self.status = 1
         else:
-            return self.COLOUR_ALIVE
+            self.status = 0
 
-    def setColour(self, inp):
-        self.colour = inp
+
+if __name__ == "__main__":
+    a = Automaton()
--- Objects/GameBoard.py
+++ Objects/GameBoard.py
@@ -2,14 +2,7 @@ import numpy as np
 import pygame
 import random
 import sys
-
-# GameBoard Object
-
-grid_size = width, height = 1280, 720
-cell_size = 10
-color_dead = 0, 0, 0
-color_alive = 255, 255, 255
-fps_max = 60
+from Objects import Automaton
 
 
 class GameBoard(object):
@@ -17,154 +10,84 @@ class GameBoard(object):
     # initialize board
     def __init__(self):
 
+        self.grid_empty_colour = 0, 0, 0
+        self.cell_dead_colour = 170, 170, 170
+        self.cell_alive_colour = 255, 255, 255
+        self.grid_size = width, height = 1280, 720
+        self.cell_size = 10
+
         self.FPSCLOCK = pygame.time.Clock()
-        self.screen = pygame.display.set_mode(grid_size)
+        self.screen = pygame.display.set_mode(self.grid_size)
         self.clear_screen()
 
-        self.last_update_completed = 0
-        self.active_grid = 0
-        self.num_cols = int(width / cell_size)
-        self.num_rows = int(height / cell_size)
-        self.grids = []
-        self.init_grids()
-        self.set_random_grid()
+        self.num_cols = int(width / self.cell_size)
+        self.num_rows = int(height / self.cell_size)
+        self.grid = []
+        self.init_grid()
         self.paused = True
         self.game_over = False
 
     # initialize grid
-    def init_grids(self):
-        # sub-function
-        def create_grid():
-            rows = []
-            for row_num in range(self.num_rows):
-                list_of_columns = [0] * self.num_cols
-                rows.append(list_of_columns)
-            return rows
-
-        self.grids.append(create_grid())
-        self.grids.append(create_grid())
-        self.active_grid = 0
-
-    def set_random_grid(self, value=None, grid=0):
-        for r in range(self.num_rows):
-            for c in range(self.num_cols):
-                if value is None:
-                    cell_value = random.choice([0, 1])
-                else:
-                    cell_value = value
-                self.grids[grid][r][c] = cell_value
+    def init_grid(self):
+        self.grid = [[Automaton.Automaton()] * self.num_cols] * self.num_rows
+        print(self.grid)
 
     def draw_grid(self):
-        # reset grid
         self.clear_screen()
-        # iterate over columns
-        for col in range(self.num_cols):
-            # iterate over rows
-            for row in range(self.num_rows):
-                # if cell is alive
-                if self.grids[self.active_grid][row][col] == 1:
-                    color = color_alive
-                else:
-                    color = color_dead
-
-                posn = (int(col * cell_size + cell_size / 2),
-                        int(row * cell_size + cell_size / 2))
-                pygame.draw.circle(self.screen, color, posn, int(cell_size / 2), 0)
+        for row in range(self.num_rows):
+            for col in range(self.num_cols):
+                posn = (int(col * self.cell_size + self.cell_size / 2),
+                        int(row * self.cell_size + self.cell_size / 2))
+                pygame.draw.circle(self.screen, self.getColour(self.grid[row][col]), posn, int(self.cell_size / 2), 0)
         pygame.display.flip()
 
     def clear_screen(self):
-        self.screen.fill(color_dead)
+        self.screen.fill(self.grid_empty_colour)
 
-    def get_cell(self, row, col):
-        return self.grids[self.active_grid][row][col]
+    def getColour(self, inp):
+        status = inp.getStatus()
+        if status == 1:
+            return self.cell_alive_colour
+        else:
+            return self.cell_dead_colour
 
-    def check_cell_neighbors(self, row_index, col_index):
-        num_alive_neighbors = 0
-        num_alive_neighbors += self.get_cell(row_index - 1, col_index - 1)
-        num_alive_neighbors += self.get_cell(row_index - 1, col_index)
-        num_alive_neighbors += self.get_cell(row_index - 1, col_index + 1)
-        num_alive_neighbors += self.get_cell(row_index, col_index - 1)
-        num_alive_neighbors += self.get_cell(row_index, col_index + 1)
-        num_alive_neighbors += self.get_cell(row_index + 1, col_index - 1)
-        num_alive_neighbors += self.get_cell(row_index + 1, col_index)
-        num_alive_neighbors += self.get_cell(row_index + 1, col_index + 1)
-
-        # Rules
-        # 1 Any live cell with fewer than two live neighbours dies, as if by underpopulation.
-        # 2 Any live cell with two or three live neighbours lives on to the next generation.
-        # 3 Any live cell with more than three live neighbours dies, as if by overpopulation.
-        # 4 Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-
-        if self.grids[self.active_grid][row_index][col_index] == 1:
-            if num_alive_neighbors > 3:
-                return 0
-            if num_alive_neighbors < 2:
-                return 0
-            if num_alive_neighbors == 2 or num_alive_neighbors == 3:
-                return 1
-        elif self.grids[self.active_grid][row_index][col_index] == 0:
-            if num_alive_neighbors == 3:
-                return 1
-        return self.grids[self.active_grid][row_index][col_index]
-
-    def update_generation(self):
-        self.set_random_grid(0, self.inactive_grid())
-        for r in range(self.num_rows - 1):
-            for c in range(self.num_cols - 1):
-                next_gen_state = self.check_cell_neighbors(r, c)
-                self.grids[self.inactive_grid()][r][
-                    c] = next_gen_state
-        self.active_grid = self.inactive_grid()
-
-    def inactive_grid(self):
-        return (self.active_grid + 1) % 2
+    def update_cells(self):
+        for row in range(self.num_rows):
+            for col in range(self.num_cols):
+                print(self.grid[row][col].switchStatus())
 
     def handle_events(self):
         for event in pygame.event.get():
+            # If Paused check for input
             if self.paused:
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if event.button == 1:
                         mousepos_x, mousepos_y = event.pos
-                        r, c = ((mousepos_x - cell_size / 2) // cell_size,
-                                (mousepos_y - cell_size / 2) // cell_size)
+                        r, c = ((mousepos_x - self.cell_size / 2) // self.cell_size,
+                                (mousepos_y - self.cell_size / 2) // self.cell_size)
                         print(event.pos, '->', (r, c))
-                        for col in range(self.num_cols):
-                            for row in range(self.num_rows):
-                                if self.grids[self.active_grid][col][row] == 1:
-                                    color = color_dead
-                                elif self.grids[self.active_grid][col][row] == 0:
-                                    color = 255, 0, 255
-                                else:
-                                    color = color_dead
-
-                        posn = (int(r * cell_size + cell_size / 2),
-                                int(c * cell_size + cell_size / 2))
+                        posn = (int(r * self.cell_size + self.cell_size / 2),
+                                int(c * self.cell_size + self.cell_size / 2))
                         print(posn)
-                        pygame.draw.circle(self.screen, color, posn, int(cell_size / 2), 0)
+                        pygame.draw.circle(self.screen, self.cell_alive_colour, posn, int(self.cell_size / 2), 0)
                 pygame.display.flip()
-
+            # Check for event
             if event.type == pygame.KEYDOWN:
                 if event.unicode == 's':
                     if self.paused:
                         self.paused = False
-                        print("live")
+                        print("Live")
                     else:
                         self.paused = True
-                        print("paused")
+                        print("Paused")
+                # Reset
                 elif event.unicode == 'r':
-                    print("randomizing the grid")
-                    self.active_grid = 0
-                    self.set_random_grid(None, self.active_grid)
-                    self.set_random_grid(0, self.inactive_grid())
-                    self.draw_grid()
-                # Quit
+                    print("Resetting Grid")
+                    self.__init__()
+                # Quit Key
                 elif event.unicode == 'q':
-                    print("Quitting the grid")
+                    print("Quitting Grid")
                     self.game_over = True
-
-            # if event is keypress of "s" then pause the loop/game.
-            # if event is keypress "r" then randomize grid
-            # if event is keypress of "q"then quit
+            # Quit Button
             if event.type == pygame.QUIT:
                 sys.exit()
-
